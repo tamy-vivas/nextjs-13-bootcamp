3690cf82408c375b36ad2e29dcc206b22d4a9c58
tamy-vivas <tamy.vivas@telusinternational.com>
2023-03-17 16:22:56 -0600

    feat: navbar and header

--- opentablenextjs/app/page.tsx
+++ opentablenextjs/app/page.tsx
@@ -6,8 +6,32 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className="text-blue-400 text-7xl">Hello World</h1>
+    <main className="bg-gray-100 min-h-screen w-screen">
+      <main className="max-w-screen-2xl m-auto bg-white">
+        {/** NAVBAR */}
+        <nav className="bg-white p-2 flex justify-between">
+          <a href="" className="font-bold text-hray-700 text-2xl">OpenTable</a>
+          <div>
+            <div className="flex">
+              <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">Sign in</button>
+              <button className="border p-1 px-4 rounded">Sign up</button>
+            </div>
+          </div>
+        </nav>
+        {/** NAVBAR */}
+
+        <main>
+          {/** HEADER */}
+
+          <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
+            <div className="text-center mt-10">
+              <h1 className="text-white text-5xl font-bolde mb-2">Find your table for any occasion</h1>
+            </div>
+          </div>
+          {/** HEADER */}
+
+        </main>
+      </main>
     </main>
   )
 }
