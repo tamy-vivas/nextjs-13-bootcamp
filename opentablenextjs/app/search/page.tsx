75e956dd28a2911624e3404f53bebde081de46d0
tamy-vivas <tamy.vivas@telusinternational.com>
2023-03-19 19:38:00 -0600

    feat: fill locations and cuisines side bar

--- opentablenextjs/app/search/page.tsx
+++ opentablenextjs/app/search/page.tsx
@@ -1,4 +1,4 @@
-import { Cuisine, PRICE, PrismaClient } from '@prisma/client';
+import { Cuisine, PRICE, PrismaClient, Location } from '@prisma/client';
 import Header from './components/Header';
 import RestaurantCard from './components/RestaurantCard';
 import SearchSideBar from './components/SearchSideBar';
@@ -33,20 +33,29 @@ const fetchRestaurantByCity = (city: string | undefined) => {
 
 }
 
+const fetchLocations = async () => {
+    return prisma.location.findMany();
+}
+
+const fetchCuisines = async () => {
+    return prisma.cuisine.findMany();
+
+}
 export default async function SearchPage({ searchParams }: { searchParams: { city: string } }) {
     const restaurants = await fetchRestaurantByCity(searchParams.city);
-
+    const locations = await fetchLocations();
+    const cuisines = await fetchCuisines();
     return (
         <>
             <Header />
             <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-                <SearchSideBar />
+                <SearchSideBar locations={locations} cuisines={cuisines} />
                 <div className="w-5/6">
                     {restaurants.length ?
                         <>
                             {
                                 restaurants.map(restaurant =>
-                                    (<RestaurantCard restaurant={restaurant} />)
+                                    (<RestaurantCard key={restaurant.id} restaurant={restaurant} />)
                                 )
                             }
                         </> : <p>Sorry, we found no restaurants in this area</p>}
