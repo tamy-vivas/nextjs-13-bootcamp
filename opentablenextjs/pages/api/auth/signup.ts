bb91031e0d94d3e8287219f30da6eb691df79731
tamy-vivas <tamy.vivas@telusinternational.com>
2023-03-21 12:53:55 -0600

    feat: jwt implemented

--- opentablenextjs/pages/api/auth/signup.ts
+++ opentablenextjs/pages/api/auth/signup.ts
@@ -2,6 +2,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import validator from "validator";
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import * as jose from "jose";
 
 const prisma = new PrismaClient();
 
@@ -68,6 +70,26 @@ export default async function handler(
         .json({ errorMessage: "Email is associated with another account" });
     }
 
-    res.status(200).json({ hello: "body" });
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const user = await prisma.user.create({
+      data: {
+        first_name: firstName,
+        last_name: lastName,
+        password: hashedPassword,
+        phone,
+        city,
+        email,
+      },
+    });
+
+    const alg = "HS256";
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+    const token = await new jose.SignJWT({ email: user.email })
+      .setProtectedHeader({ alg })
+      .setExpirationTime("24h")
+      .sign(secret);
+
+    res.status(200).json({ token });
   }
 }
