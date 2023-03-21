d6e5cce13c996e86d198507455ba0ec5ea01a55a
tamy-vivas <tamy.vivas@telusinternational.com>
2023-03-21 12:26:27 -0600

    feat: validating User's input

---
+++ opentablenextjs/pages/api/auth/signup.ts
@@ -0,0 +1,62 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import validator from "validator";
+
+type Data = {
+  name: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  if (req.method === "POST") {
+    const { firstName, lastName, email, phone, city, password } = req.body;
+    const errors: string[] = [];
+
+    const validationSchema = [
+      {
+        valid: validator.isLength(firstName, {
+          min: 1,
+          max: 20,
+        }),
+        errorMessage: "First name is Invalid!",
+      },
+      {
+        valid: validator.isLength(lastName, {
+          min: 1,
+          max: 20,
+        }),
+        errorMessage: "Last name is Invalid!",
+      },
+      {
+        valid: validator.isEmail(email),
+        errorMessage: "Email is Invalid!",
+      },
+      {
+        valid: validator.isMobilePhone(phone),
+        errorMessage: "Phone is Invalid!",
+      },
+      {
+        valid: validator.isLength(city, { min: 1 }),
+        errorMessage: "City is Invalid!",
+      },
+      {
+        valid: validator.isStrongPassword(password),
+        errorMessage: "Password is not strong enough!",
+      },
+    ];
+
+    validationSchema.forEach((check) => {
+      if (!check.valid) {
+        errors.push(check.errorMessage);
+      }
+    });
+
+    if (errors.length > 0) {
+      return res.status(400).json({ errorMessage: errors[0] });
+    }
+
+    res.status(200).json({ hello: "body" });
+  }
+}
